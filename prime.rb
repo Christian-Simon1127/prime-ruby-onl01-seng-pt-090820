1aaf3f132c4c9a44f87259cac99ce9ae68cc4542
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-19 23:18:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  split_num = num/2
+  counter = 0
+  value = false
+  while counter <= split_num
+    if number % counter == 0
+      value = true
+    end
+  end
+  value
+end
