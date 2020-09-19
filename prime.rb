d81e2c90275c7c2e77c53e72dc9d42eb2dbb161e
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-19 23:26:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,16 @@
 # Add  code here!
 def prime?(number)
   split_num = number/2
-  counter = 0
+  counter = 2
   value = true
-  while counter <= split_num
-    if number % split_num - counter == 0
-      value = false
+  if number < 0
+    value = false
+  else 
+    while counter <= split_num
+      if number % counter == 0
+        value = false
+      end
+      counter += 1
     end
-    counter += 1
-  end
   value
 end
